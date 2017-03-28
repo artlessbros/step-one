71915b15e5da7a47950abafd87e2fcf64c57a710
One of the artless bros <tom.hofman@gmail.com>
2017-03-28 17:45:12 +0200

    What's your name?

---
+++ StepOne/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StepOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.Write("What's your name? ");
+			var name = Console.ReadLine();
+			Console.WriteLine("Hello {0}!", name);
+		}
+    }
+}
