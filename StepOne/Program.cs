5b71dd06ba003b0826db6ab6c5a255987b8ea3a9
One of the artless bros <tom.hofman@gmail.com>
2017-03-28 19:50:05 +0200

    spaces

--- StepOne/Program.cs
+++ StepOne/Program.cs
@@ -2,13 +2,13 @@
 
 namespace StepOne
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+	class Program
+	{
+		static void Main(string[] args)
+		{
 			Console.Write("What's your name? ");
 			var name = Console.ReadLine();
 			Console.WriteLine("Hello {0}!", name);
 		}
-    }
+	}
 }
